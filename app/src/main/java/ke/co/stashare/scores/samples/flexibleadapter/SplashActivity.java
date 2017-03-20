23e8bc74f0c9cf154484385ee8b2e845453f75b6
Kennedy Wainaina <Kennedy Wainaina>
2017-03-20 15:42:27 +0300

    Initial commit

---
+++ app/src/main/java/ke/co/stashare/scores/samples/flexibleadapter/SplashActivity.java
@@ -0,0 +1,31 @@
+package ke.co.stashare.scores.samples.flexibleadapter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Fade;
+import android.view.Window;
+
+import ke.co.stashare.scores.utils.Utils;
+
+@SuppressWarnings("unchecked")
+public class SplashActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		if (Utils.hasLollipop()) requestWindowFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+		super.onCreate(savedInstanceState);
+
+		if (Utils.hasLollipop()) {
+			getWindow().setExitTransition(new Fade());
+		}
+
+		Intent intent = new Intent(this, MainActivity.class);
+		ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this);
+		ActivityCompat.startActivity(this, intent, options.toBundle());
+		ActivityCompat.finishAfterTransition(this);
+	}
+
+}
---
+++ app/src/main/java/ke/co/stashare/scores/samples/flexibleadapter/models/ItemModel.java
@@ -0,0 +1,15 @@
+package ke.co.stashare.scores.samples.flexibleadapter.models;
+
+/**
+ * Model item for ItemHolder.
+ *
+ * @author Davide Steduto
+ * @since 19/10/2016
+ */
+public class ItemModel extends AbstractModel {
+
+	public ItemModel(String id) {
+		super(id);
+	}
+
+}
